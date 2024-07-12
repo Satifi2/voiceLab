1d8a879c9b2961b5a90dcab64ee79a79ca03a9f4
HaiPenglai <javierriosmuqehqo81@gmail.com>
2024-07-13 00:38:58 +0800

    ground truth预处理

--- README.md
+++ README.md
@@ -7,5 +7,11 @@
 
 参考论文汇总：
 
-汉语不建议分词：[[1905.05526\] Is Word Segmentation Necessary for Deep Learning of Chinese Representations? (arxiv.org)](https://ar5iv.labs.arxiv.org/html/1905.05526)
+#### 汉语不建议分词
+
+[1905.05526\] Is Word Segmentation Necessary for Deep Learning of Chinese Representations? (arxiv.org)](https://ar5iv.labs.arxiv.org/html/1905.05526)
+
+![image-20240713002051805](./assets/image-20240713002051805.png)
+
+#### 
 
--- preprocess/generate_vocab.py
+++ preprocess/generate_vocab.py
@@ -21,17 +21,14 @@ def preprocess_vocab():
     vocab = {'pad': 0, 'bos': 1, 'eos': 2} # pad记作0的好处是，之后可能要对pad进行掩码，这样好区分
     current_index = 3
     max_tokens = 0
-    total,cnt = 0,0
+    total_sentences = 0
 
     # 词汇表不用打乱，影响小且后面都是无序的
     with open(transcript_path, 'r', encoding='utf-8') as f:
         for line in f:
-            total = total + 1
+            total_sentences += 1
             parts = line.strip().split(' ')
             characters = ''.join(parts[1:])  # 中文语音识别不分词
-            if len(characters) > 30 : #会发现绝大部分句子都很短
-                print(len(characters),characters)
-                cnt = cnt +1
             max_tokens = max(max_tokens, len(characters))
             for char in characters:
                 if char not in vocab:
@@ -47,8 +44,8 @@ def preprocess_vocab():
     with open(reverse_vocab_path, 'w', encoding='utf-8') as f:
         json.dump(reverse_vocab, f, ensure_ascii=False, indent=4)
 
-    print("文本序列最大长:", max_tokens)
-    print("总句子数:", total, "长度大于30的句子数:", cnt,"占比:",cnt/total)
+    print("句子总数:", total_sentences)
+    print("句子最大长:", max_tokens)
     print("词汇表大小:", len(vocab))
     print(f"词汇表已保存到 {vocab_path}")
     print(f"反转词汇表已保存到 {reverse_vocab_path}")
@@ -57,6 +54,11 @@ if __name__ == "__main__":
     preprocess_vocab()
 
 '''
-原来文本序列最大长: 44
-总句子数: 141600 长度大于30的句子数: 21 占比: 0.0001483050847457627
+原来句子最大长: 44
+原来句子总数: 141600 长度大于30的句子数: 21 占比: 0.0001483050847457627
+因此，可以删掉这一部分句子【如果截断和音频对应不上】，不会对模型产生影响，主要是要减少填充数量和训练时间
+
+句子总数: 141579
+句子最大长: 30
+词汇表大小: 4336
 '''
