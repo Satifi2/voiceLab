2aed22a206ab020c58ad59ff82c8f98f5b0fde66
HaiPenglai <javierriosmuqehqo81@gmail.com>
2024-07-12 23:58:28 +0800

    ground truth预处理

--- preprocess/generate_vocab.py
+++ preprocess/generate_vocab.py
@@ -18,19 +18,25 @@ def preprocess_vocab():
     os.makedirs(os.path.dirname(vocab_path), exist_ok=True)
 
     # 初始化词汇表，前三个词分别对应 pad, bos, eos，因为是中文，词汇中不会出现这三个词
-    vocab = {'pad': 0, 'bos': 1, 'eos': 2}
+    vocab = {'pad': 0, 'bos': 1, 'eos': 2} # pad记作0的好处是，之后可能要对pad进行掩码，这样好区分
     current_index = 3
+    max_tokens = 0
+    total,cnt = 0,0
 
     # 词汇表不用打乱，影响小且后面都是无序的
     with open(transcript_path, 'r', encoding='utf-8') as f:
         for line in f:
+            total = total + 1
             parts = line.strip().split(' ')
-            if len(parts) > 1:
-                characters = ''.join(parts[1:])  # 中文语音识别不分词
-                for char in characters:
-                    if char not in vocab:
-                        vocab[char] = current_index
-                        current_index += 1
+            characters = ''.join(parts[1:])  # 中文语音识别不分词
+            if len(characters) > 30 : #会发现绝大部分句子都很短
+                print(len(characters),characters)
+                cnt = cnt +1
+            max_tokens = max(max_tokens, len(characters))
+            for char in characters:
+                if char not in vocab:
+                    vocab[char] = current_index
+                    current_index += 1
 
     reverse_vocab = {index: char for char, index in vocab.items()}
 
@@ -41,8 +47,16 @@ def preprocess_vocab():
     with open(reverse_vocab_path, 'w', encoding='utf-8') as f:
         json.dump(reverse_vocab, f, ensure_ascii=False, indent=4)
 
+    print("文本序列最大长:", max_tokens)
+    print("总句子数:", total, "长度大于30的句子数:", cnt,"占比:",cnt/total)
+    print("词汇表大小:", len(vocab))
     print(f"词汇表已保存到 {vocab_path}")
     print(f"反转词汇表已保存到 {reverse_vocab_path}")
 
 if __name__ == "__main__":
     preprocess_vocab()
+
+'''
+原来文本序列最大长: 44
+总句子数: 141600 长度大于30的句子数: 21 占比: 0.0001483050847457627
+'''
