d08d94569ad35a399e6989e75056703479fedf3a
dyx <2602521797@qq.com>
2024-07-16 01:04:11 +0800

    大概7分钟可以遍历一遍数据集,256batch都只占13G显存

--- asrcn/TransformerASR.py
+++ asrcn/TransformerASR.py
@@ -47,6 +47,11 @@ class TransformerASR(nn.Module):
         
         output = self.fc_out(transformer_output)
         return output
+    
+def save_model(model, epoch, save_dir=os.path.join('..', 'model')):
+    save_path = os.path.join(save_dir, f'transformer_asr_epoch_{epoch}.pth')
+    torch.save(model.state_dict(), save_path)
+    print(f"Model saved to {save_path}")
 
 if __name__ == '__main__':
     model = TransformerASR(
@@ -61,31 +66,36 @@ if __name__ == '__main__':
     )
     model = model.to(config.device)
     criterion = nn.CrossEntropyLoss(ignore_index=0)
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.00001)
 
-    npz_file_path = os.path.join('..', 'data', 'data_aishell', 'dataset', 'train', 'S0002.npz')
-    dataset = ASRDataset(npz_file_path)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-    # test_asr_dataset(dataloader=dataloader)
-    
+    train_dir_path = os.path.join('..', 'data', 'data_aishell', 'dataset', 'train')
+    npz_files = [os.path.join(train_dir_path, f) for f in os.listdir(train_dir_path) if f.endswith('.npz')]
+
     num_epochs = 5
     for epoch in range(num_epochs):
         model.train()
         total_loss = 0
-        for wav_filenames, encoder_input, decoder_input, decoder_expected_output in dataloader:
-            encoder_input = encoder_input.to(config.device)
-            decoder_input = decoder_input.to(config.device)
-            decoder_expected_output = decoder_expected_output.to(config.device)
-            
-            optimizer.zero_grad()
-            output = model(encoder_input, decoder_input)
+        for npz_file in npz_files:
+            dataset = ASRDataset(npz_file)
+            dataloader = DataLoader(dataset, batch_size=256, shuffle=True)
+            dataset_loss = 0
+            for batch in dataloader:
+                wav_filenames, encoder_input, decoder_input, decoder_expected_output = batch
+                encoder_input = encoder_input.to(config.device)
+                decoder_input = decoder_input.to(config.device)
+                decoder_expected_output = decoder_expected_output.to(config.device)
 
-            output = output.reshape(-1, output.shape[-1])
-            decoder_expected_output = decoder_expected_output.reshape(-1)
-            loss = criterion(output, decoder_expected_output)
-            loss.backward()
-            optimizer.step()
-            
-            total_loss += loss.item()
-        
-        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(dataloader)}")
+                optimizer.zero_grad()
+                output = model(encoder_input, decoder_input)
+
+                output = output.reshape(-1, output.shape[-1])
+                decoder_expected_output = decoder_expected_output.reshape(-1)
+                loss = criterion(output, decoder_expected_output)
+                loss.backward()
+                optimizer.step()
+
+                total_loss += loss.item()
+                dataset_loss += loss.item()
+            print(f"Epoch {epoch + 1},file:{npz_file}, Loss: {total_loss / len(npz_files)}, data_set_loss:{dataset_loss}")
+        save_model(model, epoch)
+    
